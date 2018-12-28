d4c22151e00ac4fa73e0a10beb2d8fc54563ed70
James Jin <jamesj.jin@mail.utoronto.ca>
2018-12-28 16:39:44 -0500

    Added some constants

--- Packing/binpack/binpack/binpack.h
+++ Packing/binpack/binpack/binpack.h
@@ -4,5 +4,21 @@
 #pragma once
 
 #include <iostream>
+#include "pallet.h"
+#include "box.h"
 
-// TODO: Reference additional headers your program requires here.
+/* 
+	Adds a new, empty pallet of (P_LENGTH, P_WIDTH, P_HEIGHT) dimensions to a vector of open pallets
+	@Params: none
+	@Return: none
+*/
+void openNewPallet(); 
+
+/* 
+	Try to place box in container
+	@Params: 
+		item: 
+	@Return: 
+*/
+bool placeItem(const Box item, const int containerNum, vector<double> pivot);
+
