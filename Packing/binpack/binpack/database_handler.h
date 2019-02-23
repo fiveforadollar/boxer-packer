2531851dbe941790b55e005875001ba8a57883e1
William Ly <ecew.ly@mail.utoronto.ca>
2019-02-23 16:16:22 -0500

    Initialize boxes table

--- Packing/binpack/binpack/database_handler.h
+++ Packing/binpack/binpack/database_handler.h
@@ -10,23 +10,49 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 	return 0;
 }
 
-/* Dummy database initialization function */
+/*  Database table initialization function */
 int initializeDatabase() {
-	// Initiate database connection
 	sqlite3 *db;
+	char *zErrMsg = 0;
 	int rc;
+	char *sql;
 
+	/* Open database */
 	rc = sqlite3_open("test.db", &db);
 
 	if (rc) {
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-		return(1);
+		return(0);
 	}
 	else {
-		fprintf(stderr, "Opened database successfully\n");
+		fprintf(stdout, "Opened database successfully\n");
+	}
+
+	/* Create SQL statement */
+	sql = "CREATE TABLE IF NOT EXISTS BOXES("  \
+		"ID INT PRIMARY KEY     NOT NULL," \
+		"READY          INT     NOT NULL," \
+		"CAM1LEN        FLOAT," \
+		"CAM1WIDTH      FLOAT," \
+		"CAM1DIST       FLOAT," \
+		"CAM2LEN        FLOAT," \
+		"CAM2WIDTH      FLOAT," \
+		"CAM2DIST       FLOAT," \
+		"DONE           INT," \
+		"BOXSET         INT );";
+
+	/* Execute SQL statement */
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+	else {
+		fprintf(stdout, "Table created successfully\n");
 	}
 	sqlite3_close(db);
-	return(0);
+	return 0;
 }
 
 /* Function to pass SQL command to database for execution */
